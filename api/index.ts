425ea10e9f9c802a9e56922153a0fa37a0145780
Adriano Domingos <adrianonzuzinzuzi@gmail.com>
2026-06-19 09:16:08 +0100

    fix vercel runtime

--- api/index.ts
+++ api/index.ts
@@ -140,7 +140,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(helmet());
-app.use(express.json({ limit: '10kb' })); // Mitigates large payload DoS
+app.use(express.json({ limit: '10mb' }));
 
 // --- SMTP CONFIGURATION ---
 const transporter = nodemailer.createTransport({
